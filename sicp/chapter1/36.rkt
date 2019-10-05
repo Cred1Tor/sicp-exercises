470d1feb6a0a1ac9e5adb42f8fcb72e5f8f1d24c
Creditor <cred1tor@yandex.ru>
2019-10-05 09:52:32 +0600

    added 1.36

---
+++ sicp/chapter1/36.rkt
@@ -0,0 +1,31 @@
+#lang sicp
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+    (define (close-enough? v1 v2)
+          (< (abs (- v1 v2)) tolerance))
+      (define (try guess count)
+	    (let ((next (f guess)))
+	      	(display count)
+		(display " * ")
+		(display next)
+	      	(newline)
+	            (if (close-enough? guess next)
+		                next
+				          (try next (+ 1 count)))))
+        (try first-guess 1))
+
+(define (average x y)
+  (/ (+ x y)
+     2))
+
+(fixed-point (lambda (x)
+	       (/ (log 1000)
+		  (log x)))
+	     2.0) ; 34 steps
+(fixed-point (lambda (x)
+	       (average x
+			(/ (log 1000)
+			   (log x))))
+	     2.0) ; 9 steps
