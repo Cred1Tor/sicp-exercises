1a0deebd10fc501e27aa04ac97e753683a305197
Creditor <cred1tor@yandex.ru>
2019-10-08 09:48:42 +0600

    added 2.8

---
+++ sicp/chapter2/08.rkt
@@ -0,0 +1,21 @@
+#lang sicp
+
+(#%require "07.rkt")
+(#%require rackunit)
+
+(define (sub-interval a b)
+  (make-interval (- (lower-bound a)
+		    (upper-bound b))
+		 (- (upper-bound a)
+		    (lower-bound b))))
+
+(define int1 (make-interval 3 5))
+(define int2 (make-interval 4 7))
+(define int3 (make-interval -3 2))
+(define sub1 (sub-interval int1 int2))
+(define sub2 (sub-interval int1 int3))
+
+(check-equal? (lower-bound sub1) -4)
+(check-equal? (upper-bound sub1) 1)
+(check-equal? (lower-bound sub2) 1)
+(check-equal? (upper-bound sub2) 8)
