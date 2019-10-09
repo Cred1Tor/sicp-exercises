0ece8243efab3b677df5d08924c1c732b9cfbcc2
Creditor <cred1tor@yandex.ru>
2019-10-09 08:35:21 +0600

    added 2.10

---
+++ sicp/chapter2/10.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+
+(#%require "07.rkt")
+
+(define (div-interval x y)
+  (if (and (>= (upper-bound y) 0)
+	   (<= (lower-bound y) 0))
+    (error "интервал проходит через 0 - div-interval" y)
+    (mul-interval x
+		  (make-interval (/ 1.0 (upper-bound y))
+				 (/ 1.0 (lower-bound y))))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+	(p2 (* (lower-bound x) (upper-bound y)))
+	(p3 (* (upper-bound x) (lower-bound y)))
+	(p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+		   (max p1 p2 p3 p4))))
+
+(define int1 (make-interval 2 5))
+(define int2 (make-interval 1 6))
+(define int3 (make-interval -2 3))
+
+(div-interval int1 int2)
+(div-interval int1 int3)
