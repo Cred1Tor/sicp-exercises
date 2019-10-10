61886c7d643d40635ec7369e9792f2de0b159320
Creditor <cred1tor@yandex.ru>
2019-10-10 17:58:58 +0600

    added 2.25

---
+++ sicp/chapter2/25.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define x (list 1 3 (list 5 7) 9))
+
+(define y (list (list 7)))
+
+(define z (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+
+(check-equal? (car (cdr (car (cdr (cdr x))))) 7)
+(check-equal? (car (car y)) 7)
+(check-equal? (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr z)))))))))))) 7)
