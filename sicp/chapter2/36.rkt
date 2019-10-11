95f3b07dad8754cc603c9214b238b32df9434bd9
Creditor <cred1tor@yandex.ru>
2019-10-11 17:52:45 +0600

    updated 2.36

--- sicp/chapter2/36.rkt
+++ sicp/chapter2/36.rkt
@@ -6,8 +6,8 @@
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
     nil
-    (cons (accumulate op init (accumulate cons nil (map car seqs)))
-	  (accumulate-n op init (accumulate cons nil (map cdr seqs))))))
+    (cons (accumulate op init (map car seqs))
+	  (accumulate-n op init (map cdr seqs)))))
 
 (define s (list (list 1 2 3)
 		(list 4 5 6)
