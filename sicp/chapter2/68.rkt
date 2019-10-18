30b2028c4b0293458e4716b183ead36dc0679373
Creditor <cred1tor@yandex.ru>
2019-10-18 16:29:24 +0600

    optimized 2.68

--- sicp/chapter2/68.rkt
+++ sicp/chapter2/68.rkt
@@ -10,15 +10,14 @@
 	    (encode (cdr message) tree))))
 
 (define (encode-symbol symbol tree)
-    (define (get-bits current-branch acc)
-      (cond [(leaf? current-branch) acc]
+    (define (get-bits current-branch)
+      (cond [(leaf? current-branch) `()]
 	    [(pair? (memq symbol (symbols (left-branch current-branch))))
-	     (get-bits (left-branch current-branch) (append acc `(0)))]
-	    [(pair? (memq symbol (symbols (right-branch current-branch))))
-	     (get-bits (right-branch current-branch) (append acc `(1)))]))
+	     (cons 0 (get-bits (left-branch current-branch)))]
+	    [else (cons 1 (get-bits (right-branch current-branch)))]))
   (if (not (pair? (memq symbol (symbols tree))))
     (error "there's no such symbol in the tree -- ENCODE-SYMBOL" symbol)
-    (get-bits tree `())))
+    (get-bits tree )))
 
 
  (define sample-tree
