3fdeb13f9e7faa47d29160f032b2499672e4335b
Creditor <cred1tor@yandex.ru>
2019-10-18 15:23:07 +0600

    added 2.69

---
+++ sicp/chapter2/69.rkt
@@ -0,0 +1,14 @@
+#lang sicp 
+
+(#%require "67.rkt")
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge tree-set)
+  (if (= (length tree-set) 1) (car tree-set)
+    (successive-merge (adjoin-set (make-code-tree (car tree-set)
+			  		    	  (cadr tree-set))
+	  		    	  (cddr tree-set)))))
+
+(generate-huffman-tree `((B 3) (C 1) (D 1) (E 1) (A 8) (F 1) (G 1) (H 1)))
